ac720affe82d65dc91160ef7d325379485170941
Umesh <umeshtamang129@gmail.com>
2024-12-18 12:18:06 +0545

    create url link

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .import views
 
+app_name='posts'
 urlpatterns = [
     path('',views.post_list),
-    path('detail/<int:id>/',views.post_detail)
+    path('detail/<int:id>/',views.post_detail,name='detail')
 ]
