e20a0619ff2796489ee5c61e8cc44fb5ba244ed5
Umesh <umeshtamang129@gmail.com>
2024-12-18 10:23:26 +0545

    queryset using shell

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def posts_home(request):
+    queryset=Post.objects.all()
     context={
+        "queryset":queryset,
         "title":"home page...."
     }
     return render(request,'index.html',context)
