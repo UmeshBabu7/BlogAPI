9b2cb88ec54fba0e26fce6a22a49d8d6575cc38c
Umesh <umeshtamang129@gmail.com>
2024-12-18 10:56:43 +0545

    using get item or 404 query

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 # Create your views here.
 
-def posts_home(request):
+def post_list(request):
     queryset=Post.objects.all()
     context={
         "queryset":queryset,
         "title":"home page...."
     }
     return render(request,'index.html',context)
+
+def post_detail(request):
+    instance=get_object_or_404(Post,id=8)
+    context={
+        "instance":instance,
+        "title":instance.title
+
+    }
+    return render(request,'post_detail.html',context)
 
