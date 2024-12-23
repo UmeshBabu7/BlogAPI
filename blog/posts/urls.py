cc56f12a9ff72a99d75eaed18f404a63be5b8a6a
Umesh <umeshtamang129@gmail.com>
2024-12-23 14:40:55 +0545

    url links

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .import views
 
+app_name="posts"
+
 urlpatterns = [
     path('create/',views.post_create),
-    path('detail/<int:id>',views.post_detail),
+    path('detail/<int:id>',views.post_detail,name='detail'),
     path('list/',views.post_list),
     path('update/',views.post_update),
     path('delete/',views.post_delete)
