8df452828ffadcd805a8e9513c2942f7f6cc346d
Umesh <umeshtamang129@gmail.com>
2024-12-23 12:34:55 +0545

    Dyanmic Url Routing & Patterns

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('create/',views.post_create),
-    path('detail/',views.post_detail),
+    path('detail/<int:id>',views.post_detail),
     path('list/',views.post_list),
     path('update/',views.post_update),
     path('delete/',views.post_delete)
