810f47562f13d061ec50a4fbd7a028301e0cbb67
Umesh <umeshtamang129@gmail.com>
2024-12-24 08:55:57 +0545

    Basic user permission

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
@@ -10,6 +10,9 @@ from urllib.parse import quote_plus
 # Create your views here.
 
 def post_create(request):
+    if not request.user.is_staff or not  request.user.is_superuser:
+        raise Http404
+
     form=PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance=form.save(commit=False)
@@ -61,6 +64,9 @@ def post_list(request):
 
 
 def post_update(request,slug):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    
     instance=get_object_or_404(Post,slug=slug)
     form=PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
